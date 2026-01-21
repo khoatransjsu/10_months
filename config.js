2bc39dd427a61db4738190e62db9e747184ebb81
khoatransjsu <54516445+khoatransjsu@users.noreply.github.com>
2026-01-20 19:18:17 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+
+var config = {
+    lines: [
+        "Happy 10 months Anniversary Babe",
+        "Thời gian trôi qua. Mới đó mình đã quen nhau 10 tháng ",
+        "Giận nhau có, cãi vã cũng nhiều. Tụi mình cũng đã trải qua hết",
+        "Anh luôn biết ơn vì em vẫn luôn bên anh",
+        "Hãy cùng nhau biến nó thành 1 năm, 10 năm, hay 100 năm em nhé",
+        "Thương em nhiều",
+        "Khoa Tran",
+    ],
+    
+    date: "2024-12-31T20:30:00",
+  
+    names: ["Khoa", "Hailey"]
+}
